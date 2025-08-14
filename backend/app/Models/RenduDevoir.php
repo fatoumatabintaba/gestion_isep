52fc1b8cc4ba11ae9377ee8c66ba6a55ee71d8ca
fatoumatabinta ba <bintadjenga1@gmail.com>
2025-08-15 00:58:14 +0200

    creation et migration des tables

---
+++ backend/app/Models/RenduDevoir.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RenduDevoir extends Model
+{
+    use HasFactory;
+}
